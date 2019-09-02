e6048548b39fbca1ec73603be10b08e002f64891
node-toso-api <alisonsmithaccy@gmail.com>
2019-09-02 17:04:00 +0100

    sort toggleShow handler

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React, { Component } from "react";
-import Imagelayer from "./components/ImageLayer.jsx";
+// import Imagelayer from "./components/ImageLayer.jsx";
 // import Box from "./components/box";
 
 export default class App extends Component {
   state = {
-    boxes: [
-      { value: false },
-      { value: false },
-      { value: true },
-      { value: false },
-      { value: true },
-      { value: false },
-      { value: false },
-      { value: false },
-      { value: false }
-    ]
+    boxes: [false,false,false,false,false,false,false,false,false,]
   };
 
   randImage = () => {
@@ -29,10 +19,19 @@ export default class App extends Component {
     };
   };
 
-  toggleSquare = (i) => {
-    console.log(i);
-    console.log(`square clicked`);
-    this.setState({boxes[i].value: true})
+  toggleShow = (i) => {
+    console.log(`index ${i}`);
+    console.log(`square clicked`)
+    console.log(this.state.boxes[i]);
+    const newBoxes = this.state.boxes.map((box, index) => {
+      if (index === i) {
+        box = true
+      }
+      return box
+    })
+    console.log(newBoxes)
+    this.setState({boxes: newBoxes})
+
   };
 
   render() {
@@ -40,14 +39,28 @@ export default class App extends Component {
       <div
         key={i}
         className="box"
-        onClick={() => this.toggleSquare(i)}
-        style={this.getStyle(box.value)}
+        onClick={()=>this.toggleShow(i)}
+        // style={this.getStyle(box.value)}
+        style={this.getStyle(box)}
       ></div>
     ));
 
     return (
       <div className="wrapper">
-        <Imagelayer image={this.randImage()} />
+        {/* <Imagelayer image={this.randImage()} /> */}
+        <div className="container imageLayer">
+            <img className="image" src={this.randImage()} alt="cartoon" />
+            <div className="playBox">
+              <h1>Welcome to Catch-Phrase</h1>
+              <h2>Can you Guess the character???</h2>
+              <p>You have 18 points for the max correct guess & -2 for each other guess you take.</p>
+              <button className="btn">Correct</button>
+              <button className="btn">Wrong</button>
+            </div>
+            <div className="scoreBox">
+              <h1>Score: 18</h1>
+            </div>
+          </div>
 
         <div className="container gridLayer">{boxes}</div>
       </div>
