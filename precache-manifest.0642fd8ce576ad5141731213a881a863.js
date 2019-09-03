34e9890b9248586ba4173e63754884e7319c6142
cn144 <cn144@cn144s-MacBook-Pro.local>
2019-09-03 11:40:39 +0100

    Updates

---
+++ precache-manifest.0642fd8ce576ad5141731213a881a863.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb01bd747690a58841f788ec8c6aaa94",
+    "url": "/catchpicture/./index.html"
+  },
+  {
+    "revision": "963294254c6bb0d90dd1",
+    "url": "/catchpicture/./static/css/main.4325fb99.chunk.css"
+  },
+  {
+    "revision": "4b454d23e4d1de10798a",
+    "url": "/catchpicture/./static/js/2.4d917aa9.chunk.js"
+  },
+  {
+    "revision": "963294254c6bb0d90dd1",
+    "url": "/catchpicture/./static/js/main.edf1bf55.chunk.js"
+  },
+  {
+    "revision": "ce6ad4e06272cab42616",
+    "url": "/catchpicture/./static/js/runtime~main.5ffe4e76.js"
+  }
+]);
